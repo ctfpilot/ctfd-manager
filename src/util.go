c35ac95e950fbb8131d6dbbb2a5e4067a703a881
The0Mikkel <mikkel.alb@gmail.com>
2025-11-16 22:49:06 +0100

    fix: use constant time comparison for password validation

--- src/util.go
+++ src/util.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/subtle"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -41,7 +42,7 @@ func validatePassword(password string) bool {
 	valid_password = stringStrip(valid_password)
 
 	// Compare the provided password with the valid password
-	equal := strings.Compare(password, valid_password) == 0
+	equal := subtle.ConstantTimeCompare([]byte(password), []byte(valid_password)) == 0
 
 	return equal
 }
